662262e807d343215093267344044500ad537ffa
Emir Taha Kenar <kenaremirtaha@gmail.com>
2021-12-27 00:38:05 +0300

    project setup

---
+++ UDP_Protocol/KON435E_UDP/src/main.cpp
@@ -0,0 +1,37 @@
+#include <Arduino.h>
+
+#define MASTER
+//#define SLAVE
+
+  #ifdef MASTER
+  #endif
+
+  #ifdef SLAVE
+  #endif
+
+void setup() {
+  // put your setup code here, to run once:
+
+  #ifdef MASTER
+  #endif
+
+  #ifdef SLAVE
+  #endif
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  #ifdef MASTER
+  #endif
+
+  #ifdef SLAVE
+  #endif
+
+}
+
+  #ifdef MASTER
+  #endif
+
+  #ifdef SLAVE
+  #endif
